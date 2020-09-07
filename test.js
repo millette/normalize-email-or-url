e74c7b34534448810d839a7d5326ccad49604ee7
Robin Millette <robin@millette.info>
2020-09-07 19:05:33 -0400

    Fix test for ava 3.

--- test.js
+++ test.js
@@ -1,147 +1,149 @@
-'use strict'
+"use strict"
 
-import test from 'ava'
-import fn from '.'
+// import test from 'ava'
+// import fn from '.'
+const test = require("ava")
+const fn = require(".")
 
 // failures
-test('zero-0', t => {
+test("zero-0", (t) => {
   const { url, email, error } = fn()
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero-1', t => {
-  const { url, email, error } = fn(' ')
+test("zero-1", (t) => {
+  const { url, email, error } = fn(" ")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero-2', t => {
-  const { url, email, error } = fn('.')
+test("zero-2", (t) => {
+  const { url, email, error } = fn(".")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero-2b', t => {
-  const { url, email, error } = fn('gg.')
+test("zero-2b", (t) => {
+  const { url, email, error } = fn("gg.")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero', t => {
-  const { url, email, error } = fn('joe@bob.ca/john')
+test("zero", (t) => {
+  const { url, email, error } = fn("joe@bob.ca/john")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero-4', t => {
-  const { url, email, error } = fn('bob')
+test("zero-4", (t) => {
+  const { url, email, error } = fn("bob")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
-test('zero-5', t => {
-  const { url, email, error } = fn('Public Profilehttps://www.linkedin.com/in')
+test("zero-5", (t) => {
+  const { url, email, error } = fn("Public Profilehttps://www.linkedin.com/in")
   t.truthy(error)
   t.falsy(url)
   t.falsy(email)
 })
 
 // successes
-test('url with path, missing http', t => {
-  const { url, email, error } = fn('bob.ca')
+test("url with path, missing http", (t) => {
+  const { url, email, error } = fn("bob.ca")
   t.falsy(error)
-  t.is(url, 'http://bob.ca')
+  t.is(url, "http://bob.ca")
   t.falsy(email)
 })
 
-test('url with path, with http (v2)', t => {
-  const { url, email, error } = fn('http://bob.ca')
+test("url with path, with http (v2)", (t) => {
+  const { url, email, error } = fn("http://bob.ca")
   t.falsy(error)
-  t.is(url, 'http://bob.ca')
+  t.is(url, "http://bob.ca")
   t.falsy(email)
 })
 
-test('url with path, with https', t => {
-  const { url, email, error } = fn('https://bob.ca')
+test("url with path, with https", (t) => {
+  const { url, email, error } = fn("https://bob.ca")
   t.falsy(error)
-  t.is(url, 'https://bob.ca')
+  t.is(url, "https://bob.ca")
   t.falsy(email)
 })
 
-test('url with path, missing http (v3)', t => {
-  const { url, email, error } = fn('bob.ca/joe')
+test("url with path, missing http (v3)", (t) => {
+  const { url, email, error } = fn("bob.ca/joe")
   t.falsy(error)
-  t.is(url, 'http://bob.ca/joe')
+  t.is(url, "http://bob.ca/joe")
   t.falsy(email)
 })
 
-test('url: lowercase domain', t => {
-  const { url, email, error } = fn('Bob.ca/joe')
+test("url: lowercase domain", (t) => {
+  const { url, email, error } = fn("Bob.ca/joe")
   t.falsy(error)
-  t.is(url, 'http://bob.ca/joe')
+  t.is(url, "http://bob.ca/joe")
   t.falsy(email)
 })
 
-test('url: lowercase domain, cased path', t => {
-  const { url, email, error } = fn('Bob.ca/Joe')
+test("url: lowercase domain, cased path", (t) => {
+  const { url, email, error } = fn("Bob.ca/Joe")
   t.falsy(error)
-  t.is(url, 'http://bob.ca/Joe')
+  t.is(url, "http://bob.ca/Joe")
   t.falsy(email)
 })
 
-test('url containing a @ in the path', t => {
-  const { url, email, error } = fn('bob.ca/joe@john')
+test("url containing a @ in the path", (t) => {
+  const { url, email, error } = fn("bob.ca/joe@john")
   t.falsy(error)
-  t.is(url, 'http://bob.ca/joe@john')
+  t.is(url, "http://bob.ca/joe@john")
   t.falsy(email)
 })
 
-test('plain email', t => {
-  const { url, email, error } = fn('joe@bob.ca')
+test("plain email", (t) => {
+  const { url, email, error } = fn("joe@bob.ca")
   t.falsy(error)
   t.falsy(url)
-  t.is(email, 'joe@bob.ca')
+  t.is(email, "joe@bob.ca")
 })
 
-test('idn url fixed', t => {
-  const { url, email, error } = fn('www.êxample.com')
+test("idn url fixed", (t) => {
+  const { url, email, error } = fn("www.êxample.com")
   t.falsy(error)
-  t.is(url, 'http://www.xn--xample-hva.com')
+  t.is(url, "http://www.xn--xample-hva.com")
   t.falsy(email)
 })
 
-test('idn email', t => {
-  const { url, email, error } = fn('joe@xn--xample-hva.com')
+test("idn email", (t) => {
+  const { url, email, error } = fn("joe@xn--xample-hva.com")
   t.falsy(error)
   t.falsy(url)
-  t.is(email, 'joe@xn--xample-hva.com')
+  t.is(email, "joe@xn--xample-hva.com")
 })
 
-test('idn url', t => {
-  const { url, email, error } = fn('http://xn--xample-hva.com')
+test("idn url", (t) => {
+  const { url, email, error } = fn("http://xn--xample-hva.com")
   t.falsy(error)
-  t.is(url, 'http://xn--xample-hva.com')
+  t.is(url, "http://xn--xample-hva.com")
   t.falsy(email)
 })
 
-test('tiny url', t => {
-  const { url, email, error } = fn('g.g')
+test("tiny url", (t) => {
+  const { url, email, error } = fn("g.g")
   t.falsy(error)
-  t.is(url, 'http://g.g')
+  t.is(url, "http://g.g")
   t.falsy(email)
 })
 
 // FIXME: should not error, êxample should be "punycoded"
-test.skip('idn email fixed', t => {
-  const { url, email, error } = fn('joe@www.êxample.com')
+test.skip("idn email fixed", (t) => {
+  const { url, email, error } = fn("joe@www.êxample.com")
   t.falsy(error)
   t.falsy(url)
-  t.is(email, 'joe@xn--xample-hva.com')
+  t.is(email, "joe@xn--xample-hva.com")
 })
